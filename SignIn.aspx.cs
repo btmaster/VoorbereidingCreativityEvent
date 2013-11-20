e2e8f52eeee667cc445225596c3a9e3c20465bdf
btmaster <bjorn.truyen@gmail.com>
2013-11-20 21:30:46 +0100

    Twitter versie 1
    Twitter werkt

--- SignIn.aspx.cs
+++ SignIn.aspx.cs
@@ -129,8 +129,8 @@ public partial class SignIn : System.Web.UI.Page
 
 
         //Twitter
-        TwitterConnect.API_Key = "119463787-1UYWPMHNNg3YblS4D1Je36FBtdV9vDOHnP42Eowi";
-        TwitterConnect.API_Secret = "PbbNaKpM4Fr7hVUE816pOx36NBpAk9LBKqKCGMt7Vv0Kh";
+        TwitterConnect.API_Key = "7EqQPlH21pXj4yUbefEIA";
+        TwitterConnect.API_Secret = "StTNa0Wb8gKhaPrBcPDtkboJKXW6BMIGHFfhRbRshY";
         if (!IsPostBack) if (!IsPostBack)
             {
                 if (TwitterConnect.IsAuthorized)
@@ -139,8 +139,10 @@ public partial class SignIn : System.Web.UI.Page
 
                     //LoggedIn User Twitter Profile Details
                     DataTable dt = twitter.FetchProfile();
-                    imgProfile.ImageUrl = dt.Rows[0]["profile_image_url"].ToString();
-                    lblName.Text = dt.Rows[0]["name"].ToString();
+                    /*imgProfile.ImageUrl = dt.Rows[0]["profile_image_url"].ToString();
+                    var fullnaam = dt.Rows[0]["name"].ToString();
+                    var naam = fullnaam.Split(' ');
+                    lblName.Text = naam[0];
                     lblTwitterId.Text = dt.Rows[0]["Id"].ToString();
                     lblScreenName.Text = dt.Rows[0]["screen_name"].ToString();
                     lblDescription.Text = dt.Rows[0]["description"].ToString();
@@ -150,22 +152,48 @@ public partial class SignIn : System.Web.UI.Page
                     lblFavorites.Text = dt.Rows[0]["favourites_count"].ToString();
                     lblLocation.Text = dt.Rows[0]["location"].ToString();
                     tblTwitter.Visible = true;
-
+                    */
                     //Any other User Twitter Profile Details. Here jQueryFAQs
-                    dt = twitter.FetchProfile("jQueryFAQs");
-                    imgOtherProfile.ImageUrl = dt.Rows[0]["profile_image_url"].ToString();
-                    lblOtherName.Text = dt.Rows[0]["name"].ToString();
-                    lblOtherTwitterId.Text = dt.Rows[0]["Id"].ToString();
-                    lblOtherScreenName.Text = dt.Rows[0]["screen_name"].ToString();
-                    lblOtherDescription.Text = dt.Rows[0]["description"].ToString();
-                    lblOtherTweets.Text = dt.Rows[0]["statuses_count"].ToString();
-                    lblOtherFollowers.Text = dt.Rows[0]["followers_count"].ToString();
-                    lblOtherFriends.Text = dt.Rows[0]["friends_count"].ToString();
-                    lblOtherFavorites.Text = dt.Rows[0]["favourites_count"].ToString();
-                    lblOtherLocation.Text = dt.Rows[0]["location"].ToString();
-                    tblOtherTwitter.Visible = true;
+                    //dt = twitter.FetchProfile("jQueryFAQs");
+                    
 
                     btnLogin.Enabled = false;
+
+                    User newUser = new User();
+                    BLLUser BLLAddUsers = new BLLUser();
+
+                    var fullnaam = dt.Rows[0]["name"].ToString();
+                    var splitnaam = fullnaam.Split(' ');
+                    
+                    var voornaam = splitnaam[0];
+                    var naam = splitnaam[1];
+                    var email = "";
+                    var wachtwoord = "";
+                    var gebruikersnaam = dt.Rows[0]["screen_name"].ToString(); 
+                    Boolean toegestaan = false;
+
+
+                    newUser.wachtwoord = wachtwoord;
+                    newUser.gebruikersnaam = gebruikersnaam;
+                    toegestaan = BLLAddUsers.Checker(newUser);
+                    if (toegestaan == true)
+                    {
+                        lblwerkt.Text = "gebruiker bestaat al";
+                        Session.Add("gebruikersnaam", newUser.gebruikersnaam);
+                        Response.Redirect("~/Home.aspx");
+
+                    }
+                    else
+                    {
+                        newUser.email = email;
+                        newUser.voornaam = voornaam;
+                        newUser.naam = naam;
+                        newUser.rol = "visitor";
+                        BLLAddUsers.insert(newUser);
+                        Session.Add("gebruikersnaam", newUser.gebruikersnaam);
+                        Response.Redirect("~/Home.aspx");
+
+                    }
                 }
                 if (TwitterConnect.IsDenied)
                 {
